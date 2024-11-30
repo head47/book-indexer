26b42e7df2352153c6448b0362b6f2cc97c3dd52
Alex Tau <alex@lego>
2024-11-30 14:49:35 +0300

    add brief option

--- extract.py
+++ extract.py
@@ -12,6 +12,7 @@ from src.db_models import Metadata as MetadataModel
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", default=tempfile.gettempdir())
+    parser.add_argument("-b", "--brief", action="store_true", help="only output names of extracted files")
     parser.add_argument("book_ids", nargs="+")
     args = parser.parse_args()
 
@@ -24,7 +25,10 @@ def main():
         book = MetadataModel.get(MetadataModel.id == id)
         output_prefix = f"{book.title}-{book.id}-"
         output_file = extractor.extract(book.virtual_filename, output_prefix, args.output)
-        print(f"Extracted {book.title} to '{output_file}'")
+        if args.brief:
+            print(output_file)
+        else:
+            print(f"Extracted {book.title} to '{output_file}'")
 
     database.close()
 
